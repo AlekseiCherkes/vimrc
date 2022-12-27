ac5f9d169fecbaf07a25f9633b59f217fa83ee6b
Aliaksei Charkes <aliaksei.charkes@mapbox.com>
2022-12-27 21:59:07 +0300

    First attempt to vimrc

---
+++ init.lua
@@ -0,0 +1,58 @@
+require("plugins")
+require("remap")
+
+print("hello, Alex")
+
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+
+require'nvim-treesitter.configs'.setup {
+  -- A list of parser names, or "all"
+  ensure_installed = { 
+	  "help",
+	  "bash",
+	  "cmake", 
+	  "comment", 
+	  "diff", 
+	  "c", 
+	  "cpp", 
+	  "lua",
+	  "markdown",
+	  "python",
+	  "rust" 
+  },
+
+  -- Install parsers synchronously (only applied to `ensure_installed`)
+  sync_install = true,
+
+  -- Automatically install missing parsers when entering buffer
+  -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+  auto_install = false,
+
+  highlight = {
+    enable = true, -- `false` will disable the whole extension
+
+    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+    -- Using this option may slow down your editor, and you may see some duplicate highlights.
+    -- Instead of true it can also be a list of languages
+    additional_vim_regex_highlighting = false,
+  
+  incremental_selection = {
+    enable = true,
+    keymaps = {
+      init_selection = "gnn", -- set to `false` to disable one of the mappings
+      node_incremental = "grn",
+      scope_incremental = "grc",
+      node_decremental = "grm",
+    },
+  },
+  indent = {
+    enable = true
+  }
+}
+}
+
---
+++ lua/remap.lua
@@ -0,0 +1,5 @@
+print("remap")
+vim.g.mapleader = " "
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>n", vim.cmd.NERDTreeFocus)
+
