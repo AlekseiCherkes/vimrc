30d3bc46027b4dfadc337434aa2a11ebabc41331
Aliaksei Charkes <aliaksei.charkes@mapbox.com>
2023-07-19 17:32:18 +0300

    nvim-surround for vscode

--- vs-code-init.lua
+++ vs-code-init.lua
@@ -4,4 +4,11 @@
 
 vim.cmd("source ~/work/vimrc/vs-code-vimrc")
 
+require('packer').startup(function(use)
+  use('kylechui/nvim-surround')
+end)
+
+require("nvim-surround").setup({})
+
 print("OK")
+
